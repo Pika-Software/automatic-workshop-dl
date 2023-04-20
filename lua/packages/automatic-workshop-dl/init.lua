a1886f593d1644fb3e69eecc01a30794c6066ddb
PrikolMen <olegis019@gmail.com>
2023-04-21 03:02:24 +0400

    Updated

--- lua/packages/automatic-workshop-dl/init.lua
+++ lua/packages/automatic-workshop-dl/init.lua
@@ -1,4 +1,4 @@
-import( file.Exists( "packages/glua-extensions/package.lua", gpm.LuaRealm ) and "packages/glua-extensions" or "https://raw.githubusercontent.com/Pika-Software/glua-extensions/main/package.json" )
+import( gpm.sources.lua.CanImport( "packages/glua-extensions" ) and "packages/glua-extensions" or "https://raw.githubusercontent.com/Pika-Software/glua-extensions/main/package.json" )
 
 local game_GetAddonFiles = game.GetAddonFiles
 local string_StartsWith = string.StartsWith
