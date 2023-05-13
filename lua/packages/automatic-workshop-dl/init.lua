e5a0b79792f048cf4d86abdd2d8e10d7d219eb41
PrikolMen <olegis019@gmail.com>
2023-05-13 06:07:21 +0400

    gpm updated

--- lua/packages/automatic-workshop-dl/init.lua
+++ lua/packages/automatic-workshop-dl/init.lua
@@ -1,4 +1,4 @@
-import( gpm.LuaPackageExists( "packages/glua-extensions" ) and "packages/glua-extensions" or "https://github.com/Pika-Software/glua-extensions" )
+import( gpm.PackageExists( "packages/glua-extensions" ) and "packages/glua-extensions" or "https://github.com/Pika-Software/glua-extensions" )
 
 local resource_AddWorkshop = resource.AddWorkshop
 local logger = gpm.Logger
